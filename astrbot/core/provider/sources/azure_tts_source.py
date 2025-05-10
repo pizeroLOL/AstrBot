40853b83e131cdb25ac74c98b17fab0b6687bb5e
Pizero <zhaory200707@outlook.com>
2025-05-10 19:05:40 +0800

    switch to httpx

--- astrbot/core/provider/sources/azure_tts_source.py
+++ astrbot/core/provider/sources/azure_tts_source.py
@@ -1,29 +1,30 @@
 import uuid
 import xml.etree.ElementTree as Et
-from os import remove
 from pathlib import Path
 
-from azure.cognitiveservices.speech import (
-    CancellationReason,
-    ResultReason,
-    SpeechConfig,
-    SpeechSynthesizer,
-)
-from azure.cognitiveservices.speech.audio import AudioOutputConfig
+from httpx import AsyncClient
 
-from astrbot.core import logger
+from astrbot.core.config.default import VERSION
 
 from ..entities import ProviderType
 from ..provider import TTSProvider
 from ..register import register_provider_adapter
 
 TEMP_DIR = Path("data/temp")
+DEFAULT_SSML = """
+<speak version="1.0" xml:lang="zh-CN">
+    <voice name="zh-CN-YunxiaNeural">
+        <slot />
+    </voice>
+</speak>
+"""
 
 
 @register_provider_adapter("azure_tts", "Azure TTS", ProviderType.TEXT_TO_SPEECH)
 class ProviderAzureTTS(TTSProvider):
-    config: SpeechConfig
-    ssml: Et.Element | None
+    ssml: Et.Element
+    endpoint: str
+    subscription: str
 
     @staticmethod
     def __empty_str_to_none(s: str | None) -> str | None:
@@ -39,37 +40,30 @@ class ProviderAzureTTS(TTSProvider):
 
     def __init__(self, provider_config: dict, provider_settings: dict):
         super().__init__(provider_config, provider_settings)
-        region = provider_config.get("azure_tts_region", "")
-        subscription = provider_config.get("azure_tts_subscription_key", "")
-        self.config = SpeechConfig(
-            endpoint=f"https://{region if self.__empty_str_to_none(region)else 'eastasia'}.api.cognitive.microsoft.com", subscription=self.__empty_str_to_none(subscription)
-        )
+        region = self.__empty_str_to_none(provider_config.get("azure_tts_region", ""))
+        subscription = self.__empty_str_to_none(provider_config.get("azure_tts_subscription_key", ""))
+        if subscription is None:
+            raise ValueError("subscription 不可为空")
+        self.subscription = subscription
+        self.endpoint = f"https://{region if region is None else 'eastasia'}.api.cognitive.microsoft.com"
         ssml = self.__empty_str_to_none(provider_config.get("azure_tts_ssml", ""))
-        self.ssml = ssml if ssml is None else Et.fromstring(ssml)
+        self.ssml = Et.fromstring(ssml if ssml is not None else DEFAULT_SSML)
         self.set_model("azure_tts")
 
     async def get_audio(self, text: str) -> str:
         """获取文本的音频，返回音频文件路径"""
         file = TEMP_DIR / f"azure-tts-temp-{uuid.uuid4()}.wav"
-        file_text = str(file)
-        config_set = SpeechSynthesizer(
-            speech_config=self.config,
-            audio_config=AudioOutputConfig(filename=file_text),
-        )
-        result = (
-            config_set.speak_text_async(text)
-            if self.ssml is None
-            else config_set.speak_ssml_async(self.__replace_slot(self.ssml, text))
-        ).get()
-        has_file = file.is_file()
-        if result.reason == ResultReason.SynthesizingAudioCompleted and has_file:
-            return file_text
-        if has_file:
-            remove(file)
-        if result.reason != ResultReason.Canceled:
-            raise RuntimeError(f"azure_tts 未知错误 {file_text}, result: {result}")
-        cancellation_details = result.cancellation_details
-        logger.error(f"azure_tts 取消生成: {cancellation_details.reason}")
-        if cancellation_details.reason == CancellationReason.Error:
-            logger.error(f"azure_tts 错误信息: {cancellation_details.error_details}")
-        raise RuntimeError(f"azure_tts 生成错误 {file_text}")
+        headers = {
+            "Ocp-Apim-Subscription-Key": self.subscription,
+            "Content-Type": "application/ssml+xml",
+            "User-Agent": f"astrboot/{VERSION}",
+            "X-Microsoft-OutputFormat": "raw-48khz-16bit-mono-pcm"
+        }
+        async with AsyncClient() as client:
+            rsp = await client.post(self.endpoint, content=self.__replace_slot(self.ssml.__copy__() ,text),headers=headers)
+            if rsp.status_code != 200:
+                raise RuntimeError(f"azure tts 状态码错误，{rsp.status_code}：{rsp.text}")
+            with file.open("wb") as o:
+                async for chunk in rsp.aiter_bytes(4096):
+                    o.write(chunk)
+            return str(file)
